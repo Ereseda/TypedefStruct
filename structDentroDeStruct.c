b14288d8c998ad8de4bc52eb15817fe319a42d73
Eli Santos Reseda <50843398+Ereseda@users.noreply.github.com>
2023-04-03 12:40:32 -0300

    Struct dentro de Struct
    incluindo uma struct dentro de outra struct

---
+++ structDentroDeStruct.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+//Aula 169: Utilizando Struct de struct
+//https://www.youtube.com/watch?v=tadaZD97Gtc&list=PLqJK4Oyr5WShSqejWzL5oMuujnHFeHhSC&index=3
+//Atividade do dia 03/04/2023
+typedef struct{
+    int dia, mes, ano;
+}DataNas;
+typedef struct{
+    DataNas dataNas;
+    int idade;
+    char sexo;
+    char nome[100];
+}Pessoa;
+
+int main(){
+
+    Pessoa pessoa1;
+
+    printf("Digite seu nome: ");
+    fgets(pessoa1.nome,100,stdin);
+    printf("Digite sua idade: ");
+    scanf("%d", &pessoa1.idade);
+    scanf("%c");
+    printf("Digite F ou M para o sexo: ");
+    scanf("%c", &pessoa1.sexo);
+    //SE  eu colocar o nome aqui, vou ter que limpar o buffer do teclado então vou colocar o nome  a cima.
+    printf("Digite sua data de nascimento no formato dd mm aaaa.");
+    scanf("%d%d%d", &pessoa1.dataNas.dia, &pessoa1.dataNas.mes, &pessoa1.dataNas.ano);
+    //Imprime os dados
+    printf("Nome: %s\nIdade: %d\nSexo: %c\n", pessoa1.nome, pessoa1.idade, pessoa1.sexo);
+
+    printf("Data de nasc.: %d/%d/%d\n", pessoa1.dataNas.dia, pessoa1.dataNas.mes, pessoa1.dataNas.ano);
+    return 0;
+}
+
