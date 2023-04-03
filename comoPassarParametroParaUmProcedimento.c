e0ef878673dd4768a7a9178d691ecb2d5e9a0d70
Eli Santos Reseda <50843398+Ereseda@users.noreply.github.com>
2023-04-03 14:43:58 -0300

    Passando parâmetro para procedimento
    Como passar parâmetro para procedimento em Struct.

---
+++ comoPassarParametroParaUmProcedimento.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+//Atividade do dia 03/04/2023
+//https://www.youtube.com/watch?v=EJbIsx3iplU&list=PLqJK4Oyr5WShSqejWzL5oMuujnHFeHhSC&index=4
+typedef struct{
+    int dia, mes, ano;
+}DataNas;
+typedef struct{
+    DataNas dataNas;
+    int idade;
+    char sexo;
+    char nome[100];
+}Pessoa;
+
+void imprimirPessoa(Pessoa p){
+    printf("\tNome: %s", p.nome);
+    printf("\tIdade: %d\n", p.idade);
+    printf("\tSexo: %c", p.sexo);
+    printf("\tData de nasc.: %d/%d/%d\n", p.dataNas.dia, p.dataNas.mes, p.dataNas.ano);
+}
+int main(){
+
+    Pessoa pessoa1;
+    DataNas data;
+    printf("%d\n", sizeof(pessoa1));
+    printf("Digite seu nome: ");
+    fgets(pessoa1.nome,100,stdin);
+    printf("Digite sua idade: ");
+    scanf("%d", &pessoa1.idade);
+    scanf("%c");
+    printf("Digite F ou M para o sexo: ");
+    scanf("%c", &pessoa1.sexo);
+    //SE  eu colocar o nome aqui, vou ter que limpar o buffer do teclado então vou colocar o nome  a cima.
+    printf("Digite sua data de nascimento no formato dd mm aaaa.");
+    scanf("%d%d%d", &pessoa1.dataNas.dia, &pessoa1.dataNas.mes, &pessoa1.dataNas.ano);
+//    Imprime os dados
+    printf("Nome: %s\nIdade: %d\nSexo: %c\n", pessoa1.nome, pessoa1.idade, pessoa1.sexo);
+    printf("Data de nasc.: %d/%d/%d\n", pessoa1.dataNas.dia, pessoa1.dataNas.mes, pessoa1.dataNas.ano);
+    imprimirPessoa(pessoa1);
+    return 0;
+}
+
+
+
